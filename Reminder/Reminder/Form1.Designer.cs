8417958c19956229f811692b4cc7e35683f6a930
Alexander <cool.selutin99@yandex.ru>
2017-12-19 21:21:45 +0400

    Add files via upload

---
+++ Reminder/Reminder/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Reminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ReminderIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ContextStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AddStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.ContextStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ReminderIcon
+            // 
+            this.ReminderIcon.ContextMenuStrip = this.ContextStrip;
+            this.ReminderIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("ReminderIcon.Icon")));
+            this.ReminderIcon.Text = "Reminder";
+            this.ReminderIcon.Visible = true;
+            // 
+            // ContextStrip
+            // 
+            this.ContextStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddStrip,
+            this.ExitStrip});
+            this.ContextStrip.Name = "ContextStrip";
+            this.ContextStrip.Size = new System.Drawing.Size(136, 48);
+            // 
+            // AddStrip
+            // 
+            this.AddStrip.Name = "AddStrip";
+            this.AddStrip.Size = new System.Drawing.Size(135, 22);
+            this.AddStrip.Text = "Развернуть";
+            this.AddStrip.Click += new System.EventHandler(this.AddStrip_Click);
+            // 
+            // ExitStrip
+            // 
+            this.ExitStrip.Name = "ExitStrip";
+            this.ExitStrip.Size = new System.Drawing.Size(135, 22);
+            this.ExitStrip.Text = "Выход";
+            this.ExitStrip.Click += new System.EventHandler(this.ExitStrip_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(123, 43);
+            this.inputTextBox.Mask = "00:00";
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(66, 31);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.ValidatingType = typeof(System.DateTime);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите время:";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(74, 92);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(164, 24);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Включить напоминание";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(117, 122);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 157);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Напоминалка";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ContextStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon ReminderIcon;
+        private System.Windows.Forms.MaskedTextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip ContextStrip;
+        private System.Windows.Forms.ToolStripMenuItem AddStrip;
+        private System.Windows.Forms.ToolStripMenuItem ExitStrip;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Reminder/Reminder/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reminder
+{
+    public partial class Form1 : Form
+    {
+        public static bool flag = false;
+        public static bool flagForIcon = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (inputTextBox.MaskCompleted)
+            {
+                DialogResult result = MessageBox.Show("У Вас остались несохранённые изменения! Останитесь здесь?", "Внимание", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                switch (result)
+                {
+                    case System.Windows.Forms.DialogResult.Yes:
+                        e.Cancel = true;
+                        break;
+                    case System.Windows.Forms.DialogResult.No:
+                        ReminderIcon.Visible = false;
+                        inputTextBox.Clear();
+                        Close();
+                        break;
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!inputTextBox.MaskCompleted)
+            {
+                MessageBox.Show("Введите корректное значение!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                inputTextBox.Clear();
+            }
+            else if(validateTime())
+            {
+                Visible = false;
+                ShowInTaskbar = false;
+                ReminderIcon.Visible = true;
+                timer1.Enabled = true;
+                flag = true;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                inputTextBox.Clear();
+            }
+        }
+
+        public bool validateTime()
+        {
+            char [] time = inputTextBox.Text.ToCharArray();
+            string hours = time[0] +""+ time[1];
+            string minutes = time[3] + "" + time[4];
+            int Inthours=0, Intminutes=0;
+            if (hours.Equals("00"))
+            {
+                Inthours = 0;
+            }
+            else if (minutes.Equals("00"))
+            {
+                Intminutes = 0;
+            }
+            else
+            {
+                 Inthours = int.Parse(hours);
+                 Intminutes = int.Parse(minutes);
+            }
+
+            if (Inthours <= 23 && Intminutes <= 59)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void iconClick()
+        {
+            Visible = true;
+            ShowInTaskbar = true;
+            ReminderIcon.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                string resultHours, resultMinutes, resultTime;
+                DateTime currTime = DateTime.Now;
+
+                resultHours = currTime.TimeOfDay.Hours.ToString();
+                resultMinutes = currTime.TimeOfDay.Minutes.ToString();
+                resultTime = resultHours + ":" + resultMinutes;
+
+                if (inputTextBox.Text.Equals(resultTime))
+                {
+                    flag = false;
+                    inputTextBox.Clear();
+                    //ReminderIcon.ShowBalloonTip(30000, "Оповещение", "Время!", ToolTipIcon.Info);
+                    MessageBox.Show("Время!","Оповещение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    iconClick();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Visible)
+            {
+                ReminderIcon.Visible = true;
+                iconClick();
+            }
+        }
+
+        private void AddStrip_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Visible = true;
+            ShowInTaskbar = true;
+            ReminderIcon.Visible = false;
+        }
+
+        private void ExitStrip_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            inputTextBox.Clear();
+            Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label2.Text= DateTime.Now.ToLongTimeString();
+        }
+    }
+}
